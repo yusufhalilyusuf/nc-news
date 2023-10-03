94a8ba351c32eb2a715fac0bf18502e945d2c76d
yusufhalilyusuf <yusufhalilyusuf@hotmail.com>
2023-10-03 10:31:04 +0100

    completed task4

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controller");
 const { getAllEndpoints } = require("./controllers/api.controller");
+const { getArticlesById } = require("./controllers/articles.controller");
+const { customErrorHandler } = require("./controllers/errors.controller");
 
 const app = express();
 
 app.get("/api/topics", getTopics);
 app.get("/api", getAllEndpoints);
+app.get("/api/articles/:article_id", getArticlesById);
+
+
+app.use(customErrorHandler);
 
 app.all("/*", (req, res, next) => {
   res.status(404).send({ message: "path not found" });
